994a78e66388f57ed545cf1e6e1e2df1f6b04b14
chenxustu1 <chenxustu1@qq.com>
2020-11-23 18:36:09 +0800

    Add project files.

---
+++ blazor_component/blazor_vs_extensionPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+ 
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$
+    {
+        public $safeitemname$()
+        {
+          
+        }
+    }
+}
---
+++ blazor_component/blazor_vs_extensionPage.service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bridge.Html5;
+
+
+
+public static partial class DataService
+{
+
+}
